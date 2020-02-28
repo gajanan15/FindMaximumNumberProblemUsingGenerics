37fcb3773e9a883c661a56f50a6bbdba0d5b8962
gajanan15 <gajanandesai97@gmail.com>
2020-02-28 17:14:15 +0530

    REFACTORED : UC3_All 3 Test Cases Into One Generic Method

--- src/main/java/com/largestnumber/MaxNumber.java
+++ src/main/java/com/largestnumber/MaxNumber.java
@@ -1,35 +1,15 @@
 package com.largestnumber;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class MaxNumber {
-    public Integer getLargestNumber(Integer firstNumber,Integer secondNumber,Integer thirdNumber) {
-        Integer max=firstNumber;
+    public <E extends Comparable> E getLargest(E firstNumber,E secondNumber, E thirdNumber) {
+        E max=firstNumber;
         if(secondNumber.compareTo(max)>0){
             max=secondNumber;
         }
         if(thirdNumber.compareTo(max)>0){
            max=thirdNumber;
-        }
-        return max;
-    }
-
-    public Float getLargestNumberUsingFloat(Float firstNumber, Float secondNumber, Float thirdNumber) {
-        Float max=firstNumber;
-        if(secondNumber.compareTo(max)>0){
-            max=secondNumber;
-        }
-        if(thirdNumber.compareTo(max)>0){
-            max=thirdNumber;
-        }
-        return max;
-    }
-
-    public String getLargestString(String firstString, String secondString, String thirdString) {
-        String max=firstString;
-        if(secondString.compareTo(max)>0){
-            max=secondString;
-        }
-        if(thirdString.compareTo(max)>0){
-            max=thirdString;
         }
         return max;
     }
--- src/test/java/com/checklargestnumber/LargestNumberTest.java
+++ src/test/java/com/checklargestnumber/LargestNumberTest.java
@@ -9,54 +9,55 @@ public class LargestNumberTest {
 
     @Test
     public void givenThreeInteger_FirstIntegerIsMaximum_ShouldReturnFirstInteger() {
-        int largestNumber = maxNumber.getLargestNumber(30, 20, 10);
+        int largestNumber = maxNumber.getLargest(30, 20, 10);
         Assert.assertEquals(30,largestNumber);
+
     }
 
     @Test
     public void givenThreeInteger_SecondIntegerIsMaximum_ShouldReturnSecondInteger() {
-        int largestNumber = maxNumber.getLargestNumber(10, 30, 20);
+        int largestNumber = maxNumber.getLargest(10, 30, 20);
         Assert.assertEquals(30,largestNumber);
     }
 
     @Test
     public void givenThreeInteger_ThirdIntegerIsMaximum_ShouldReturnThirdInteger() {
-        int largestNumber = maxNumber.getLargestNumber(10, 20, 30);
+        int largestNumber = maxNumber.getLargest(10, 20, 30);
         Assert.assertEquals(30,largestNumber);
     }
 
     @Test
     public void givenThreeFloatValue_WhenFirstFloatValueIsMaximum_ShouldReturnFirstFloatValue() {
-        Float largestNumber = maxNumber.getLargestNumberUsingFloat(30.5f, 20.5f, 10.5f);
+        float largestNumber = maxNumber.getLargest(30.5f, 20.5f, 10.5f);
         Assert.assertEquals(30.5f,largestNumber,0.0);
     }
 
     @Test
     public void givenThreeFloatValue_WhenSecondFloatValueIsMaximum_ShouldReturnSecondFloatValue() {
-        Float largestNumber = maxNumber.getLargestNumberUsingFloat(10.5f, 30.5f, 20.5f);
+        float largestNumber = maxNumber.getLargest(10.5f, 30.5f, 20.5f);
         Assert.assertEquals(30.5f,largestNumber,0.0);
     }
 
     @Test
     public void givenThreeFloatValue_WhenThirdFloatValueIsMaximum_ShouldReturnThirdFloatValue() {
-        Float largestNumber = maxNumber.getLargestNumberUsingFloat(10.5f, 20.5f, 30.5f);
+        float largestNumber = maxNumber.getLargest(10.5f, 20.5f, 30.5f);
         Assert.assertEquals(30.5f,largestNumber,0.0);
     }
 
     @Test
     public void givenThreeStrings_WhenFirstStringIsMaximum_ShouldReturnFirstString() {
-        String largestString = maxNumber.getLargestString("Peach", "Apple", "Banana");
+        String largestString = maxNumber.getLargest("Peach", "Apple", "Banana");
         Assert.assertEquals("Peach",largestString);
     }
 
     @Test
     public void givenThreeStrings_WhenSecondStringIsMaximum_ShouldReturnSecondString() {
-        String largestString = maxNumber.getLargestString("Apple", "Peach", "Banana");
+        String largestString = maxNumber.getLargest("Apple", "Peach", "Banana");
         Assert.assertEquals("Peach",largestString);
     }
     @Test
     public void givenThreeStrings_WhenThirdStringIsMaximum_ShouldReturnThirdString() {
-        String largestString = maxNumber.getLargestString("Apple", "Banana", "Peach");
+        String largestString = maxNumber.getLargest("Apple", "Banana", "Peach");
         Assert.assertEquals("Peach",largestString);
     }
 }
