d11a431671e9e8959391b95fa7c66b1986317401
gajanan15 <gajanandesai97@gmail.com>
2020-02-28 19:39:03 +0530

    ADDED:Extend The getLargest method To Take More Than Three Parameter

--- src/main/java/com/largestnumber/MaxNumber.java
+++ src/main/java/com/largestnumber/MaxNumber.java
@@ -1,4 +1,8 @@
 package com.largestnumber;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MaxNumber<E extends Comparable> {
     E firstNumber;
     E secondNumber;
@@ -14,13 +18,19 @@ public class MaxNumber<E extends Comparable> {
         return getLargest(firstNumber,secondNumber,thirdNumber);
     }
 
-    public <E extends Comparable> E getLargest(E firstNumber, E secondNumber, E thirdNumber) {
+    public static  <E extends Comparable> E getLargest(E firstNumber, E secondNumber, E thirdNumber,E...option) {
         E max=firstNumber;
         if(secondNumber.compareTo(max)>0){
             max=secondNumber;
         }
         if(thirdNumber.compareTo(max)>0){
            max=thirdNumber;
+        }
+        if(option.length!=0){
+            Arrays.sort(option, Collections.reverseOrder());
+            if(max.compareTo(option[0])<0){
+                max=option[0];
+            }
         }
         return max;
     }
--- src/test/java/com/checklargestnumber/LargestNumberTest.java
+++ src/test/java/com/checklargestnumber/LargestNumberTest.java
@@ -6,42 +6,45 @@ import org.junit.Test;
 
 public class LargestNumberTest {
 
+    // Test Integer Values
     @Test
     public void givenThreeInteger_FirstIntegerIsMaximum_ShouldReturnFirstInteger() {
-        MaxNumber maxNumber = new MaxNumber(30, 20, 10);
-        Assert.assertEquals(30,maxNumber.getLargest());
+        MaxNumber maxNumber = new MaxNumber(50,40,30);
+        Assert.assertEquals(50,maxNumber.getLargest());
     }
 
     @Test
     public void givenThreeInteger_SecondIntegerIsMaximum_ShouldReturnSecondInteger() {
-        MaxNumber maxNumber = new MaxNumber(10, 30, 20);
-        Assert.assertEquals(30,maxNumber.getLargest());
+        MaxNumber maxNumber = new MaxNumber(40,50,30);
+        Assert.assertSame(50,maxNumber.getLargest());
     }
 
     @Test
     public void givenThreeInteger_ThirdIntegerIsMaximum_ShouldReturnThirdInteger() {
-        MaxNumber maxNumber = new MaxNumber(10, 20, 30);
-        Assert.assertEquals(30,maxNumber.getLargest());
+        MaxNumber maxNumber = new MaxNumber(40, 30, 50);
+        Assert.assertSame(50,maxNumber.getLargest());
     }
 
+    // Test Float Values
     @Test
     public void givenThreeFloatValue_WhenFirstFloatValueIsMaximum_ShouldReturnFirstFloatValue() {
-        MaxNumber maxNumber = new MaxNumber(30.5f, 20.5f, 10.5f);
-        Assert.assertEquals(30.5f, maxNumber.getLargest());
+        MaxNumber maxNumber = new MaxNumber<>(30.5f,15.5f,12.5f);
+        Assert.assertEquals(30.5f,maxNumber.getLargest());
     }
 
     @Test
     public void givenThreeFloatValue_WhenSecondFloatValueIsMaximum_ShouldReturnSecondFloatValue() {
-        MaxNumber maxNumber = new MaxNumber(10.5f, 30.5f, 20.5f);
+        MaxNumber maxNumber = new MaxNumber(15.5f,30.5f,12.5f);
         Assert.assertEquals(30.5f,maxNumber.getLargest());
     }
 
     @Test
     public void givenThreeFloatValue_WhenThirdFloatValueIsMaximum_ShouldReturnThirdFloatValue() {
-        MaxNumber maxNumber = new MaxNumber(10.5f, 20.5f, 30.5f);
+        MaxNumber maxNumber = new MaxNumber(30.5f,15.5f,12.5f);
         Assert.assertEquals(30.5f,maxNumber.getLargest());
     }
 
+    // Test String
     @Test
     public void givenThreeStrings_WhenFirstStringIsMaximum_ShouldReturnFirstString() {
         MaxNumber maxNumber = new MaxNumber("Peach", "Apple", "Banana");
@@ -53,9 +56,26 @@ public class LargestNumberTest {
         MaxNumber maxNumber = new MaxNumber("Apple", "Peach", "Banana");
         Assert.assertEquals("Peach",maxNumber.getLargest());
     }
+
     @Test
     public void givenThreeStrings_WhenThirdStringIsMaximum_ShouldReturnThirdString() {
-        MaxNumber maxNumber = new MaxNumber("Apple", "Banana", "Peach");
+        MaxNumber maxNumber = new MaxNumber("Banana", "Apple", "Peach");
         Assert.assertEquals("Peach",maxNumber.getLargest());
+    }
+
+    // Test More Than Three Parameters
+    @Test
+    public void givenMoreThanThreeInteger_WhenIntegerIsMaximum_ShouldReturnMaximumInteger() {
+        Assert.assertSame(50,MaxNumber.getLargest(50,40,30,20,10));
+    }
+
+    @Test
+    public void givenMoreThanThreeFloatValue_WhenFloatValueIsMaximum_ShouldReturnMaximumFloatValue() {
+        Assert.assertEquals(30.5f, MaxNumber.getLargest(30.5f,15.5f,12.5f,5.5f,1.1f),0.0f);
+    }
+
+    @Test
+    public void givenMoreThanThreeStrings_WhenStringIsMaximum_ShouldReturnMaximumString() {
+        Assert.assertEquals("Peach",MaxNumber.getLargest("Banana", "Apple", "Peach","Mango"));
     }
 }
